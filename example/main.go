319faf7e7e8afc1ad5c111b303b33196984e74f6
linqiankai <417350372@qq.com>
2023-02-09 15:54:48 +0800

    例子全部不要用真实的

--- example/main.go
+++ example/main.go
@@ -11,8 +11,8 @@ import (
 func main() {
 
 	err := remote.SetOptions(&remote.Option{
-		Url:         "10.43.0.12",
-		Port:        8848,
+		Url:         "localhost",
+		Port:        8080,
 		NamespaceId: "77b74411-f615-479e-a24e-6e848bc19121",
 		GroupName:   "JXEU-CSMS",
 		Config:      remote.Config{DataId: "jxeu-acos.properties"},
@@ -22,7 +22,7 @@ func main() {
 		panic(err)
 	}
 	remoteViper := viper.New()
-	err = remoteViper.AddRemoteProvider("nacos", "http://10.43.0.12:8848", "")
+	err = remoteViper.AddRemoteProvider("nacos", "localhost:8080", "")
 	if err != nil {
 		panic(err)
 	}
